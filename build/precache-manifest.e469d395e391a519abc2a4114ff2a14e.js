9cc43fff251118f7c33972738bb1b3a37d59656b
Aadesh Jain <27aadesh@gmail.com>
2020-07-06 14:23:17 +0530

    updated build script

---
+++ build/precache-manifest.e469d395e391a519abc2a4114ff2a14e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb3e0ee363c251a2e41a8cf94ddb920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ccfbbabb281f3c9ae53",
+    "url": "/static/css/main.fad707e9.chunk.css"
+  },
+  {
+    "revision": "71774acfca4a866c6981",
+    "url": "/static/js/2.b58c3f80.chunk.js"
+  },
+  {
+    "revision": "6ccfbbabb281f3c9ae53",
+    "url": "/static/js/main.3fa4cb28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
